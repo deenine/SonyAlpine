54db1c8eed347f4a9dad5570d66d8774c039c6d5
Doug Chivers <doug.chivers@hp.com>
2015-01-21 17:41:27 +0000

    Added usb keyboard emulation, fixed stability issues.`

--- SonyAlpine.c
+++ SonyAlpine.c
@@ -14,7 +14,12 @@ bottom	        48800	0.84
 
 /** General Config                                                            **/
 const int sonyPin = A0;
+const int ledPin = 13;
 const int alpPin = 2;
+const int arrowDelay = 250; //repeat rate for arrow key presses
+const int enterDelay = 350; //repeat rate for enter/esc key presses
+
+boolean headunit = true; //True if stalk is controlling HU, false if emulating kb
 
 /** Alpine Codes                                                              **/
 char alpVolUp[] = "110101111101101110101011110110111101011011010101";
@@ -27,34 +32,44 @@ char alpTrackDown[] = "110101111101101110101011010111011111101011010101";
 char alpPstUp[] = "110101111101101110101011101010111110111101010101";
 char alpPstDown[] = "110101111101101110101011010101011111111101010101";
 
-
 /** Sony Voltages                                                             **/
 const int sonySrc = 41; //4.1v
 const int sonyAtt = 34;
 const int sonyOff = 49;
 const int sonyVolDown = 14;
 const int sonyVolUp = 18;
-const int sonySeekDown = 22;
-const int sonySeekUp = 26;
+const int sonySeekDown = 26;
+const int sonySeekUp = 22;
 const int sonyBack = 29;
 const int sonyFront = 11;
 const int sonyBottom = 8;
 
 
+/** Setup                                                                    **/
 
 void setup() {
-  pinMode(2,OUTPUT);
+  pinMode(alpPin,OUTPUT);
+  pinMode(ledPin,OUTPUT);
   digitalWrite(2,LOW);
-  Serial.begin(9600);
-  Serial.println("hello world");
+  Keyboard.begin();
 }
 
+/** Main program loop                                                        **/
 void loop() {
   float sonyPress = analogRead(sonyPin);
   float voltage = (sonyPress / 1024) * 5.0;
   if (voltage > 0.01) {
+    delay(10); //filter out the initial flutter in voltage
+    //take a second reading
+    sonyPress = analogRead(sonyPin);
+    voltage = (sonyPress / 1024) * 5.0;
     checkVoltage(int((voltage) * 10));
   }
+
+  if(headunit == true) 
+    digitalWrite(ledPin,HIGH);
+  else
+    digitalWrite(ledPin,LOW);
 }
 
 void checkVoltage(int voltage) {
@@ -63,61 +78,95 @@ void checkVoltage(int voltage) {
     case sonySrc:
       sendCode(alpSource);
       return;
-    case sonyAtt:
-      sendCode(alpAtt);
-      return;
-    case sonyOff:
-      sendCode(alpOff);
-      return;
     case sonyVolDown:
       sendCode(alpVolDown);
       return;
     case sonyVolUp:
       sendCode(alpVolUp);
       return;
+    case sonyBottom:
+      if (headunit == true)
+        headunit = false;
+      else
+        headunit = true;  
+      delay(500);
+      return;      
+
+
+    case sonyAtt:
+      if (headunit)
+        sendCode(alpAtt);
+      else
+        sendKey(KEY_RETURN);
+      return;
+    case sonyOff:
+      if (headunit)
+        sendCode(alpOff);
+      else
+        sendKey(KEY_ESC);
+      return;
     case sonySeekUp:
-      sendCode(alpTrackUp);
+      if (headunit)
+        sendCode(alpTrackUp);
+      else
+        sendKey(KEY_RIGHT_ARROW);
       return;
     case sonySeekDown:
-      sendCode(alpTrackDown);
+      if (headunit)
+        sendCode(alpTrackDown);
+      else
+        sendKey(KEY_LEFT_ARROW);
       return;
     case sonyBack:
-      sendCode(alpPstDown);
+      if (headunit)
+        sendCode(alpPstDown);
+      else
+        sendKey(KEY_DOWN_ARROW);
       return;
     case sonyFront:
-      sendCode(alpPstUp);
+      if (headunit)
+        sendCode(alpPstUp);
+      else
+        sendKey(KEY_UP_ARROW);
       return;
-    case sonyBottom:
-      break;      
     default:
       break;
   }
 }
 
+void sendKey(int key){
+  Keyboard.write(key);
+  //longer delay between enter/return than arrow keys.
+  if (key == KEY_RETURN || key == KEY_ESC)
+    delay(enterDelay);
+  else
+    delay(arrowDelay);
+  return;
+  
+}
 
 void sendCode(char *code) {
-  const int ALPINELENGTH = 47;
-  const int ALPINERATE = 500; //microseconds
-  const int ALPINEINTERVAL = 41; //milliseconds
-  const int alpineHighInnit = 8000; //microseconds
-  const int alpineLowInnit = 4500; //microseconds
+  const int alpCodeLength = 47;
+  const int alpRate = 500; //microseconds
+  const int alpInterval = 41; //milliseconds
   
   //Innit  
-  digitalWrite(2,HIGH);
+  digitalWrite(alpPin,HIGH);
   delay(8); // 8 ms high init
-  digitalWrite(2,LOW);
+  digitalWrite(alpPin,LOW);
   delayMicroseconds(4500); // 4.5 ms low init
   
-  for (int i = 0; i <= 47; i++) {
+  //Send command
+  for (int i = 0; i <= alpCodeLength; i++) {
     if (code[i] == '1') {
-      digitalWrite(2,HIGH);
+      digitalWrite(alpPin,HIGH);
     } else {
-      digitalWrite(2,LOW);
+      digitalWrite(alpPin,LOW);
     }
-      delayMicroseconds(500);
-      digitalWrite(2,LOW);
-      delayMicroseconds(500);
+      delayMicroseconds(alpRate);
+      digitalWrite(alpPin,LOW);
+      delayMicroseconds(alpRate);
   }
-  digitalWrite(2,LOW);
-  delay(41);  
+  digitalWrite(alpPin,LOW);
+  delay(41); //41ms interval between commands  
 }
